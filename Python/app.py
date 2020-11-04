1d57ba0afef7052adfd66137a0c1757f9544be3d
Gary Ortiz <goritz1490@gmail.com>
2020-11-03 22:02:58 -0600

    integracion go->rabbitmq->python->redis

--- Python/app.py
+++ Python/app.py
@@ -5,20 +5,30 @@ from flask import Flask, redirect, url_for, request, render_template, jsonify
 import requests
 from json import loads,  dumps
 from pymongo import MongoClient
+import redis
 app = Flask(__name__)
 
 bod =""
 def callback(ch, method, properties, body):
         print(" [x] Recibido %r" % body.decode())
-        myclient = MongoClient('mongodb://mongo:27017/',username='root',password='rootpassword')
+
+        #insercion base de datos mongo
+        myclient = MongoClient('mongodb://localhost:27017/',username='root',password='rootpassword')
         mydb = myclient["mydb"]
         mycol = mydb["casos"]
-        mydict = { "name": "Peter", "address": "Lowstreet 27" }
+        x = mycol.insert_one(loads(body.decode())) 
+        #insercion base de datos redis
+        redisc = redis.StrictRedis(host='localhost', port=6379,db=0,charset="utf-8", decode_responses=True)
+        parsed = loads(body.decode())
+        redisc.rpush('casos', str(parsed))
 
-        x = mycol.insert_one(loads(body.decode()))            
+        #recuperado = redis.mget('caso')
+        #print ("tipo de variable de recuperado: "+str(recuperado))
+
+                   
 def main(): 
     
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
     channel.queue_declare(queue='Case')
