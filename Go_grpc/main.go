c0e19ea0d3301846911bdb149733a66f348d2725
Gary Ortiz <goritz1490@gmail.com>
2020-11-04 13:20:01 -0600

    grcp con go y python, mongo y redis

---
+++ Go_grpc/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	//"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+	//"html/template"
+	"io/ioutil"
+	"net/http"
+	//"os"
+	"strconv"
+	//"strings"
+	"github.com/gorilla/mux"
+	"log"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+//struct de casos
+type caso struct {
+    Name string `json:"name"`
+	Location string `json:"location"`
+	Age int `json:"age"`
+	Infectedtype string `json:"infectedtype"`
+	State string `json:"state"`
+}
+
+const (
+	address = "localhost:50051"
+)
+func CrearCaso(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+        panic(err)
+    }
+    var c caso
+    err = json.Unmarshal(body, &c)
+    if err != nil {
+        panic(err)
+	}
+	var jsonStr = string(`{"name":"` + c.Name + `","location":"` + c.Location +`","age":`+strconv.Itoa(c.Age)+`,"infectedtype":"`+c.Infectedtype+`","state":"`+c.State+ `"}`)
+	//grpc
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+        log.Fatal("no se conecto: %w",err)
+	}
+	defer conn.Close()
+	ca := pb.NewGreeterClient(conn)
+	//strjson := jsonStr
+	ctx, cancel := context.WithTimeout(context.Background(),time.Second)
+	defer cancel()
+	//enviando 
+	ra, error := ca.SayHello(ctx, &pb.HelloRequest{Name: jsonStr})
+	if error != nil{
+		log.Fatal("error al enviar el mensaje: %w ",error)
+	}
+	log.Printf("respuesta: %s", ra.GetMessage())
+
+}
+
+
+func main() {
+	//rutas del servidor
+
+	router := mux.NewRouter()
+    router.HandleFunc("/NewCaso", CrearCaso).Methods("POST")
+	router.HandleFunc("/", Inicio).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", router))
+
+	
+}
+
+func Inicio(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Servidor GOLAND ENCENDIDO para GRPC!!!")
+}
---
+++ Python-grpc/app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import sys
+import os
+from flask import Flask, redirect, url_for, request, render_template, jsonify
+from json import loads,  dumps
+from pymongo import MongoClient
+import redis
+from concurrent import futures
+import logging
+
+import grpc
+import helloworld_pb2
+import helloworld_pb2_grpc
+app = Flask(__name__)
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+    def SayHello(self,request,context):
+        print("mensaje: %s!" % request.name )
+        ## enviamos a mongo
+        myclient = MongoClient('mongodb://localhost:27017/',username='root',password='rootpassword')
+        mydb = myclient["mydb"]
+        mycol = mydb["casos"]
+        x = mycol.insert_one(loads(request.name)) 
+        ## enviamos a redis
+        redisc = redis.StrictRedis(host='localhost', port=6379,db=0,charset="utf-8", decode_responses=True)
+        parsed = loads(request.name)
+        redisc.rpush('casos', str(parsed))
+
+        return helloworld_pb2.HelloReply(message = 'hola %s'%request.name)
+                   
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+    
+
+    
+
+if __name__ == '__main__':
+    try:
+        logging.basicConfig()
+        main()        
+    except KeyboardInterrupt:
+        print('Finalizo Programa')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
